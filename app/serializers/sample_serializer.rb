79f4ac0c88a04fe65594d9e13e904d4b468c583f
elinag <elina.gorshkova@icloud.com>
2019-09-11 21:55:04 -0400

    Clear the code
    Delete unused files

--- app/serializers/sample_serializer.rb
+++
@@ -1,3 +0,0 @@
-class SampleSerializer < ActiveModel::Serializer
-  attributes :id, :name, :author, :sounds
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :samples
-  resources :favorites, except: %i[new edit]
-  resources :collections, except: %i[new edit]
   # RESTful routes
+  resources :collections, except: %i[new edit]
   resources :examples, except: %i[new edit]
 
   # Custom routes
